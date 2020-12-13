eb77a7f87a7081466b9d780387b8f8281f661e0b
Marek <b3.marek@gmail.com>
2020-12-13 11:09:29 +0100

    Add FormTest - firstName

--- src/test/java/Day4/FormTest.java
+++ src/test/java/Day4/FormTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
@@ -46,6 +47,29 @@ WebDriver driver;
         List<WebElement> yearsOfExpirience = driver.findElements(By.name("gridRadiosExperience"));
         getRandomElement(yearsOfExpirience).click();
 
+        WebElement manualTester = driver.findElement(By.id("gridCheckManulTester"));
+        manualTester.click();
+
+        //continents
+        WebElement continentsElements = driver.findElement(By.id("selectContinents"));
+        Select continents = new Select(continentsElements);
+        List<WebElement> continentsOptions = continents.getOptions();
+        continentsOptions.remove(0);
+        WebElement randomOption = getRandomElement(continentsOptions);
+        continents.selectByVisibleText(randomOption.getText());
+
+        //commands
+        WebElement commandsElement = driver.findElement(By.id("selectSeleniumCommands"));
+        Select commandsSelect = new Select(commandsElement);
+        //commandsSelect.selectByValue("browser-commands");
+        List<WebElement> commandsOptions = commandsSelect.getOptions();
+        int howManyOption = getRandomNumber(commandsOptions.size());
+        for (int i = 0; i < howManyOption; i++) {
+            commandsSelect.selectByIndex(i);
+        }
+
+
+        //AdditionalInformation
         WebElement additionalInfo = driver.findElement(By.cssSelector("#additionalInformations"));
         additionalInfo.sendKeys("Test");
 
@@ -58,5 +82,11 @@ WebDriver driver;
         Random rnd = new Random();
         int randomNumber = rnd.nextInt(elemnts.size() - 1);
         return elemnts.get(randomNumber);
+    }
+
+    public int getRandomNumber(int max){
+        Random rnd = new Random();
+        int randomNumber = rnd.nextInt(max)+1;
+        return randomNumber;
     }
 }
