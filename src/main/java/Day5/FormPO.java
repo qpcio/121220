d5c23641e46f47d0115e9b55c5841d75a35dd80f
qpcio <qpcio@onet.eu>
2020-12-19 11:14:23 +0100

    rozwiazanie pierwszego PO

--- src/main/java/Day5/FormPO.java
+++ src/main/java/Day5/FormPO.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
+import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 public class FormPO {
     WebDriver driver;
@@ -22,6 +25,87 @@ public class FormPO {
 
     @FindBy(name = "gridRadiosExperience")
     private List<WebElement> yearsOfExperience;
+
+    @FindBy(css = "input[value='male']")
+    private WebElement maleRadioButton;
+
+    @FindBy(id = "gridCheckManulTester")
+    private WebElement manualTesterGrid;
+
+    @FindBy(id = "selectContinents")
+    private WebElement selectContinents;
+
+    @FindBy(id = "selectSeleniumCommands")
+    private WebElement commands;
+
+    @FindBy(id = "chooseFile")
+    private WebElement fileInput;
+
+    @FindBy(id = "additionalInformations")
+    private WebElement additionalInformationTextBox;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(id = "validator-message")
+    private WebElement validatorMessage;
+
+    @FindBy(id = "inputAge3")
+    private WebElement ageTextBox;
+
+    public void selectRandomExperience(){
+        getRandomElement(yearsOfExperience).click();
+    }
+
+    public void fillAge(int age){
+        ageTextBox.sendKeys(Integer.toString(age));
+    }
+
+    public String getValidatorMessageText(){
+        return validatorMessage.getText();
+    }
+
+
+    public void submit(){
+        submitButton.click();
+    }
+
+
+    public void fillAdditionalInformation(String text){
+        additionalInformationTextBox.sendKeys(text);
+    }
+
+
+    public void fillFileInput(){
+        File file = new File("src/main/resources/emptyFile.txt");
+        fileInput.sendKeys(file.getAbsolutePath());
+    }
+
+    public void selectRandomCommand(){
+        Select commandSelect = new Select(commands);
+        List<WebElement> commandsOptions = commandSelect.getOptions();
+        int howManyOption = getRandomNumber(commandsOptions.size());
+        for (int i = 0; i < howManyOption; i++) {
+            commandSelect.selectByIndex(i);
+        }
+    }
+
+
+    public void selectRandomContinent(){
+        Select continents = new Select(selectContinents);
+        List<WebElement> continentsOptions = continents.getOptions();
+        continentsOptions.remove(0);
+        WebElement randomOption = getRandomElement(continentsOptions);
+        continents.selectByVisibleText(randomOption.getText());
+    }
+
+    public void selectManualTester(){
+        manualTesterGrid.click();
+    }
+
+    public void selectMale(){
+        maleRadioButton.click();
+    }
 
 
     public void fillName(String firstname, String lastName){
@@ -44,4 +128,26 @@ public class FormPO {
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
+
+    private WebElement getRandomElement(List<WebElement> elemnts){
+        Random rnd = new Random();
+        int randomNumber = rnd.nextInt(elemnts.size() - 1);
+        return elemnts.get(randomNumber);
+    }
+
+    private int getRandomNumber(int max){
+        Random rnd = new Random();
+        int randomNumber = rnd.nextInt(max)+1;
+        return randomNumber;
+    }
+
+    private void veryBadSleep() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/test/java/Day5/FirstPOTests.java
+++ src/test/java/Day5/FirstPOTests.java
@@ -1,20 +1,28 @@
 package Day5;
 
 import Day4.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstPOTests extends TestBase {
     @Test
     public void firstTest(){
+        //given
         FormPO formPO = new FormPO(driver);
         formPO.openMe();
+        //when
         formPO.fillName("Bogdan","Wiadro");
         formPO.fillEmail("test@test.com");
-        //tu nowe metody az do konca
-
-
-        //a tu asercja - jak?! 
-
-
+        formPO.selectMale();
+        formPO.fillAge(45);
+        formPO.selectRandomExperience();
+        formPO.selectManualTester();
+        formPO.selectRandomContinent();
+        formPO.selectRandomCommand();
+        formPO.fillFileInput();
+        formPO.fillAdditionalInformation("Tralalala");
+        formPO.submit();
+        //then
+        Assert.assertEquals(formPO.getValidatorMessageText(),"Form send with success");
     }
 }
