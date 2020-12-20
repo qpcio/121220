8846351c3bc4ddec6b92bc43d4f88706ea33226f
qpcio <qpcio@onet.eu>
2020-12-20 13:36:12 +0100

    new test with subPOs

---
+++ src/test/java/Day5/MyShopTests/ShopProcessTests.java
@@ -0,0 +1,14 @@
+package Day5.MyShopTests;
+
+import Day4.TestBase;
+import org.testng.annotations.Test;
+
+public class ShopProcessTests extends TestBase {
+
+    @Test
+    public void shouldCorrectlyAddMultipleItemsToCartTest(){
+        // 3 razy wylosuj produkt i ilosc -> dodaj do koszyka
+        // przejdz do koszyka
+        // sprawdz wartosc i prawislowos (istnienie) dobrych obiektow w koszyku
+    }
+}
